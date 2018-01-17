0e3a698c7783c1d73ada0ffd5a47f5e31308706b
Cody Schindler <admin@codyschindler.com>
2018-01-17 14:05:36 -0800

    almost done with phone verification

--- server/sms.js
+++ server/sms.js
@@ -3,25 +3,68 @@ const lib = require('../lib');
 const express = require('express');
 const router = express.Router();
 
-const authenticate = (req, res) => {
-  res.status(200).end()
+const getPhoneNumberParts = (phoneNumber) => {
+  if (phoneNumber.length === 10) {
+    return {phone_number: phoneNumber, country_code: '1'}
+  } 
+  else if (phoneNumber.length === 11) {
+    return { phone_number: phoneNumber.slice(1), country_code: phoneNumber[0]}    
+  }
+  else {
+    return false;
+  }
 }
 
-const submitForVerification = (req, res) => {
+const verify = (req, res) => {
+  let phoneDetails = getPhoneNumberParts(req.body.p)
+  if (phoneDetails.phone_number && phoneDetails.country_code && req.body.token) {
+    lib.verify.verifyPhoneToken(phoneDetails.phone_number, phoneDetails.country_code, req.body.token)
+      .then(response => {
+        console.log('Confirm phone success confirming code: ', response);
+        if (response.success) {
+          db.updatePhoneNumber(req.body.p, req.body.uid)
+          .then(resp => {
+            db.verifyPhone(resp.id, resp.phone)
+            res.status(200).json(response);
+          })
+          .catch(err => {
+            console.log('error updating database', err);
+            res.status(200).json(err);
+          })
+        } 
+        else { res.status(200).json(err) }//got response but was unsuccessful
+      })
+      .catch(err => {
+        console.log('error creating phone reg request', err);
+        res.status(500).json(err);
+      })
+  } 
+  else {
+    console.log('Failed in Confirm Phone request body: ', req.body);
+    res.status(500).json({ error: "Missing fields" });
+  }
+};
+
+const request = (req, res) => {
   console.log('verifying phone number', req.query.p, req.query.uid)
+  let phoneDetails = getPhoneNumberParts(req.query.p);
+  if (!phoneDetails) {res.status(500).end()} //error if phone number is invalid length
   db.updatePhoneNumber(req.query.p, req.query.uid)
-  .then(response => {console.log(response)}).catch(err=>console.log(err))
-  // lib.twillio.verifyNumber(req.params.p)
-  //   .then(response => {
-    //   console.log('phone number verified', response)
-    //   res.status(200).json({
-    //     verified: response
-    //   });
-    // })
-    // .catch(err => {
-    //   console.log(err);
-      res.status(500).end();
-    // });
+    .then((row) => {
+      console.log('successfully updated database for user', row)
+    })
+    .catch(err => {
+      console.log('could not update user in db', err)
+    })
+  lib.verify.requestPhoneVerification(phoneDetails.phone_number, phoneDetails.country_code)
+    .then(response => {
+      console.log('Success register phone API call: ', response);
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      console.log('error creating phone reg request', err);
+      res.status(500).json(err);
+    })
 };
 
 const getUserPhone = (req, res) => {
@@ -37,8 +80,8 @@ const getUserPhone = (req, res) => {
     });
 };
 
-router.get('/verify', submitForVerification);
-router.get('/authenticate/:number', authenticate);
+router.post('/verification/verify', verify);
+router.get('/verification/start', request);
 router.get('/userphone/:userId', getUserPhone);
 
 module.exports = router;
